ae12d5f1e12753876306fa6ac8cf57d3835a245b
Sairaj Sonawane <96881739+SairajSonawane@users.noreply.github.com>
2023-01-19 21:36:29 +0530

    Add files via upload

---
+++ Sumoftwonumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class keyword
+{public static void main(String[] args)
+    {
+       
+        Scanner s=new Scanner(System.in);
+        int a,b,c;
+         a=s.nextInt();
+         b=s.nextInt();
+         c=s.nextInt();
+        c=a+b;
+        System.out.println("sum of:"+c);
+    }
+}
