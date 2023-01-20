d2771e58c866b8bdb5503eefa857cc6bdf6b513f
Sairaj Sonawane <96881739+SairajSonawane@users.noreply.github.com>
2023-01-20 11:11:44 +0530

    Add files via upload

---
+++ Greeting.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.Scanner;
+import java.util.*;
+
+class name
+{
+    /**
+     * @param args
+     */
+    public static void main(String[] args) 
+    {
+        Scanner z= new Scanner(System.in);
+        String name;
+        System.out.println("May I know your name?");
+        name=z.nextline();
+        System.out.print("Welcome Mr/Miss" +name);
+
+    }
+
+
+}
