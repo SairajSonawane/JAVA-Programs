2b506305b9bebc016c7c636ce560920c41201fbb
Sairaj Sonawane <96881739+SairajSonawane@users.noreply.github.com>
2023-01-27 11:43:18 +0530

    Add files via upload

---
+++ youngornot.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+public class youngornot 
+{ public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int age;
+        System.out.println("Enter age :");
+        age=sc.nextInt();
+        if(age>=20)
+        {System.out.println("NOT YOUNG");}
+        else
+        {System.out.println("YOUNG");}
+    
+    }
+    
+}
