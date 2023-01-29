7a3de04ec4f0219b1b9a65f4ef521229ab14d700
Sairaj Sonawane <96881739+SairajSonawane@users.noreply.github.com>
2023-01-29 12:57:56 +0530

    Add files via upload

---
+++ sumofnnumbers.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.*;
+import java.util.Scanner;
+class numbers
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner (System.in);
+        
+        System.out.println("Enter n number : ");
+        int n=sc.nextInt();
+        int sum=0;
+        
+        for (int i=0; i<=n; i++) {
+            sum= sum+i;
+        }
+        
+        System.out.println("Sum");
+        
+    }
+}
